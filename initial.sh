e326499b7c72d47380d75ec7e01eabb581969751
silentijsje <github@silentijsje.com>
2024-06-10 15:57:08 +0200

    Building a new code server

--- initial.sh
+++ initial.sh
@@ -2,16 +2,34 @@
 
 # curl -fsSL https://raw.githubusercontent.com/silentijsje/script_lib/main/initial.sh | sh
 
-read -p "Enter the name for the ssh key: " SSH_KEY_NAME
 
-sudo apt update
-sudo apt install ansible
-sudo apt install nano
-sudo apt install git
+SSH_KEY_NAME="id_ed25519"
+
+# Get the hostname of the PC
+HOSTNAME=$(hostname)
+
+sudo apt -y update
+sudo apt -y install ansible
+sudo apt -y install nano
+sudo apt -y install git
 
 git config --global user.name "silentijsje"
 git config --global user.email "github@silentijsje"
 
-ssh-keygen -t ed25519 -N "" -f .ssh/$SSH_KEY_NAME -C $SSH_KEY_NAME -q
+# Check if the SSH key already exists
+if [ ! -f ~/.ssh/${SSH_KEY_NAME} ]; then
+    ssh-keygen -t ed25519 -N "" -f ~/.ssh/${SSH_KEY_NAME} -C ${HOSTNAME} -q
+    cat ~/.ssh/${SSH_KEY_NAME}.pub
+else
+    echo "SSH key ~/.ssh/${SSH_KEY_NAME} already exists. Skipping key generation."
+fi
 
 curl -fsSL https://tailscale.com/install.sh | sh
+
+mkdir ~/github
+cd ~/github
+git clone git@github.com:silentijsje/script_lib.git
+git clone git@github.com:silentijsje/homelab.git
+
+# Print the hostname
+echo "The hostname of this PC is: $HOSTNAME"
