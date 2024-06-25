889faf542d97e8f263c4cfc9ea68af2eecf3dba5
Stanley <github@silentijsje.com>
2024-06-25 16:57:31 +0200

    Refactor fetchgit.sh script for improved functionality and readability

--- fetchgit.sh
+++ fetchgit.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-cd /home/gamer0308/github/homelab
+# Define an array of directories
+directories=(
+    "/home/gamer0308/github/homelab"
+    "/home/gamer0308/scripts_lib"
+)
 
-# Fetch the remote repository
-echo "$(date --utc +%FT%TZ): Fetching remote repository..."
-git fetch
+# Iterate over each directory
+for dir in "${directories[@]}"; do
+    echo "Processing directory: $dir"
+    cd "$dir" || exit
 
-# Set the upstream branch
-UPSTREAM=${1:-'@{u}'}
+    # Fetch the remote repository
+    echo "$(date --utc +%FT%TZ): Fetching remote repository..."
+    git fetch
 
-# Get the local, remote, and base commit hashes
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+    # Set the upstream branch
+    UPSTREAM=${1:-'@{u}'}
 
-# Check if the local and remote branches are the same
-if [ $LOCAL = $REMOTE ]; then
-    # If the local and remote branches are the same, the repository is up-to-date
-    echo "$(date --utc +%FT%TZ): Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    # If the local branch is based on the same commit as the remote branch, changes are detected
-    BUILD_VERSION=$(git rev-list HEAD)
-    echo "$(date --utc +%FT%TZ): Changes detected, deploying new version $BUILD_VERSION"
-    /home/gamer0308/github/script_lib/deploy.sh
-elif [ $REMOTE = $BASE ]; then
-    # If the remote branch is based on the same commit as the local branch, local changes are detected
-    echo "$(date --utc +%FT%TZ): Local changes detected, stashing..."
-    git stash
-    /home/gamer0308/github/script_lib/deploy.sh
-else
-    # If the local and remote branches have diverged, the repository is in a diverged state
-    echo "$(date --utc +%FT%TZ): Diverged"
-fi
+    # Get the local, remote, and base commit hashes
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    # Check if the local and remote branches are the same
+    if [ "$LOCAL" = "$REMOTE" ]; then
+        echo "$(date --utc +%FT%TZ): Up-to-date"
+    elif [ "$LOCAL" = "$BASE" ]; then
+        BUILD_VERSION=$(git rev-list HEAD)
+        echo "$(date --utc +%FT%TZ): Changes detected, deploying new version $BUILD_VERSION"
+        /home/gamer0308/github/script_lib/deploy.sh
+    elif [ "$REMOTE" = "$BASE" ]; then
+        echo "$(date --utc +%FT%TZ): Local changes detected, stashing..."
+        git stash
+        /home/gamer0308/github/script_lib/deploy.sh
+    else
+        echo "$(date --utc +%FT%TZ): Diverged"
+    fi
+done
